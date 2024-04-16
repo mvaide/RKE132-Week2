d86401e07d1a6179ade6f61fe047ab35d71ec3fe
Morris Vaide <mvaide@tktk.ee>
2024-04-16 18:10:00 +0300

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutajal sisestada oma vanuse
+//kui kasutaja on väiksem kui 13, siis konsoolis kuvatakse:
+//"You are too young"
+//muul juhul 
+//konsoolis kuvatakse "Welcome!"
+
+Console.WriteLine("Enter your age:");
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+//boolean - ture/false
+
+bool isAgeNumber = int.TryParse(userAge, out userAgeNum);
+
+Console.WriteLine($"Parse result {isAgeNumber}. User is {userAgeNum} years old.");
+
+if (isAgeNumber == true)
+{
+    if (userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome!");
+    }
+    else
+    {
+        Console.WriteLine("You are too young!");
+    }
+}
+else
+{
+    Console.WriteLine("Could not read your age.");
+}
+
+
+//if (userAge >= 13)
+//{
+//    Console.WriteLine("Welcome!");
+//}
+//else 
+//{
+//    Console.WriteLine("You are too young!");
+//}
---
+++ UserGender Validation/Program.cs
@@ -0,0 +1,28 @@
+﻿//rakendus küsib kasutajalt valida tema sugu (m/f)
+//rakendus küsib kasutajalt sisestada tema perekonnanime
+//lähtudes kasutaja valikust, rakendus tervitab kasutajat järgmiselt:
+//"Welcome, Mr [kasutaja perekonnanimi]" / "Welcome, Ms [kasutaja perekonnanimi]"
+
+
+
+Console.WriteLine("Please, select your gender (m/f)");
+
+char userGender = char.Parse(Console.ReadLine()); //Loeb konsoolist maha andmeid string (sõne) format
+
+Console.WriteLine($"Please, enter your last name:");
+string userLastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
+
+
